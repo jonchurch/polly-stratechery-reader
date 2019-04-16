ec067ef2fb73c68c3745240b035fb0753fe11277
Jon Church <me@jonchurch.com>
2019-04-16 09:16:46 -0400

    change default story to aggregation theory article

--- index.js
+++ index.js
@@ -6,20 +6,22 @@ const chunk = require("lodash.chunk");
 const { speak, synthesize } = require("./polly");
 
 const URL =
-  process.argv[2] || "https://stratechery.com/2019/the-value-chain-constraint/";
+  process.argv[2] || "https://stratechery.com/2015/aggregation-theory/";
 axios
-  .get(URL)
-  .then(response => cheerio.load(response.data))
+  .get(URL) // get the blog page
+  .then(response => cheerio.load(response.data)) // load the html
   .then($ => {
+    // transform the text
     $("time.updated").remove();
-
     const allText = $("article")
       .children()
       .text();
+    // chunk the text up to send to polly
     const textChunks = chunk(allText, 3000);
     const audioBlobs = textChunks.map(chunk => synthesize(chunk.join("")));
     Promise.all(audioBlobs).then(blobs => {
       const bigBlob = Buffer.concat(blobs);
+      // speak the finished audio blob
       speak(bigBlob);
     });
   })
