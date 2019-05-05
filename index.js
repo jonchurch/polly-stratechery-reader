46e420ae038a5f9d567b3fc513c1a2c1010cee31
Jon Church <me@jonchurch.com>
2019-05-04 20:41:40 -0400

    use article slug for filename, save to output file if mp3 on cli

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const chunk = require("lodash.chunk");
 const fs = require("fs");
+const url = require("url");
 
 const { speak, synthesize } = require("./polly");
+
+const captureTitleFromUrl = url => {
+  const reg = /.*\/(\w+(?:\-\w+)?)[?\.]?/;
+  const title = reg.exec(url);
+  return title[1];
+};
 
 const URL =
   process.argv[2] || "https://stratechery.com/2015/aggregation-theory/";
 const outputFormat = process.argv[3] || "pcm";
+
 axios
   .get(URL, {
     headers: {
@@ -30,11 +38,13 @@ axios
     );
     return Promise.all(audioBlobs)
       .then(blobs => {
+        const urlTitle = captureTitleFromUrl(URL);
+
         const bigBlob = Buffer.concat(blobs);
         if (outputFormat === "pcm") {
           speak(bigBlob);
         } else {
-          saveToDisk(bigBlob);
+          saveToDisk(bigBlob, urlTitle);
         }
         // speak the finished audio blob
       })
@@ -42,13 +52,12 @@ axios
   })
   .catch(err => console.error(err));
 
-function saveToDisk(audioData, dest = "./out.mp3") {
-  // const outStream = fs.createWriteStream(dest);
-  // audioData.pipe(writeStream);
-  // outStream.on("end", () => {
-  //   console.log("writing finished!");
-  // });
-  fs.writeFile(dest, audioData, err => {
+function saveToDisk(audioData, fileName) {
+  if (!fileName) {
+    throw new Error("File destination required before saving to disk");
+  }
+  const destination = `./output/${fileName}.mp3`;
+  fs.writeFile(destination, audioData, err => {
     if (!err) {
       console.log("Success");
     } else {
